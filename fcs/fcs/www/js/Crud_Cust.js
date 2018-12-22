33e5c3c56e63883c4b938f821d92060db80b4d98
Yash <yash.talegaonkar37@gmail.com>
2018-12-22 22:20:04 +0530

    insertdata() function 10%

--- fcs/fcs/www/js/Crud_Cust.js
+++ fcs/fcs/www/js/Crud_Cust.js
@@ -1,29 +1,60 @@
 function insertData(){
   var cname = document.getElementById('name').value;
   var pno = document.getElementById('pno').value;
-  var cgen = document.getElementById('cgen').value;
+  var cust_gender =document.getElementById('cust_gender').value;
+  var cust_addr = document.getElementById('').value;
+  var cust_mob=document.getElementById('').value;
+  var cust_blood_group =document.getElementById('').value;
+  var cust_desc =document.getElementById('').value;
+  var cust_reference=document.getElementById('').value;
+  var cust_fit_his=document.getElementById('').value;
+  var cust_medic_his=document.getElementById('').value;
+  var joining_date =document.getElementById('').value;
+  var cust_ending_date =document.getElementById('').value;
 
   //var users = firebase.database().ref("users/");
 var firebaseRef = firebase.database().ref(); // connection to database
 firebaseRef.child("users/").push({    // inserting data in realtime {
 
-  customer_name:"yash",
-  customer_gender:"male",
-  customer_addr:"pune",
-  customer_mob:"999999",
-  customer_blood_group:"a+",
-  customer_desc:"blahlha",
-  cust_how_info:"from friend",
-  cust_quest:"NO, i dont know",
-  cust_quest_level:"1",
-  cust_medic:"None",
-  cust_fit_history:"2",
+  customer_name:cname,
+  customer_gender:cust_gender,
+  customer_addr:cust_addr,
+  customer_mob:cust_mob,
+  customer_blood_group:cust_blood_group,
+  customer_desc:cust_desc,
+  customer_reference:cust_reference,
+  //cust_quest:,
+//  cust_quest_level:"1",
+  cust_medic:cust_medic_his,
+  cust_fit_history:cust_fit_his,
+  cust_join_date:joining_date,
+  cust_end_date:cust_ending_date
 
 }
 );
 }
 
 /*
+Data(){
+cname
+pno
+cust_gender
+cust_addr
+cust_mob
+cust_blood_group
+cust_desc
+cust_reference
+cust_fit_his
+cust_medic_his
+joining_date
+cust_ending_date
+}
+
+
+
+
+
+
 function insert_cust()
 {
 customer_name;
