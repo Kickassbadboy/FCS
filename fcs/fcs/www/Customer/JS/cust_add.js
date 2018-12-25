5718311fdf158631ccef82079ca41ad44fb2045c
Yash <yash.talegaonkar37@gmail.com>
2018-12-25 17:12:45 +0530

    created stub for admin_add.html and basic code

--- fcs/fcs/www/Customer/JS/cust_add.js
+++ fcs/fcs/www/Customer/JS/cust_add.js
@@ -1,26 +1,26 @@
 function insertData(){
   var cname = document.getElementById('name').value;
-  var cust_gender =document.getElementById('radio').value;
+  //var cust_gender =document.getElementById('radio').value;
   var cust_addr = document.getElementById('address').value;
   var cust_mob=document.getElementById('mobile').value;
   var cust_blood_group =document.getElementById('bg').value;
-  var cust_desc =document.getElementById('').value;
-  var cust_reference=document.getElementById('').value;
-  var cust_fit_his=document.getElementById('').value;
-  var cust_medic_his=document.getElementById('').value;
-  var joining_date =document.getElementById('').value;
-  var cust_ending_date =document.getElementById('').value;
-
+  var cust_email =document.getElementById('email').value;
+  var cust_reference=document.getElementById('cust_refer').value;
+  var cust_fit_his=document.getElementById('cust_fit_hist').value;
+  var cust_medic_his=document.getElementById('medic_hist').value;
+  var joining_date =document.getElementById('joining_date').value;
+  var cust_ending_date =document.getElementById('ending_date').value;
+  var cust_occupation =document.getElementById('occupation').value;
   //var users = firebase.database().ref("users/");
 var firebaseRef = firebase.database().ref(); // connection to database
 firebaseRef.child("users/").push({    // inserting data in realtime {
-  customer_id:cust_id,
+
   customer_name:cname,
-  customer_gender:cust_gender,
+
   customer_addr:cust_addr,
   customer_mob:cust_mob,
   customer_blood_group:cust_blood_group,
-  customer_desc:cust_desc,
+  customer_email:cust_email,
   customer_reference:cust_reference,
   //cust_quest:,
 //  cust_quest_level:"1",
